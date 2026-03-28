ecd7e313dc540df63223df4deda0b2b212bcc6dc
ivy-helix <miyata.hikaru.jp@gmail.com>
2026-03-28 21:57:06 +0900

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'GUNPEY',
-        short_name: 'GUNPEY',
-        description: 'Gunpey puzzle game',
-        theme_color: '#060202',
-        background_color: '#060202',
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
